8a9c2fca0105e652916eddac403a1aaa1b798963
Chris Davis <defbyte@gmail.com>
2023-04-10 16:07:20 -0400

    Simplify path config

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,9 +1,7 @@
 # Custom $PATH
 
-export USR_PATH="/usr/local/bin:/usr/local/sbin"
-export OSX_PATH="/usr/bin:/usr/sbin:/bin:/sbin"
 export NVM_DIR="$HOME/.nvm"
 export PYENV_ROOT="$HOME/.pyenv"
 export VSCODE_PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
-export PATH="$PYENV_ROOT/bin:$VSCODE_PATH:$USR_PATH:$OSX_PATH"
+export PATH="$PYENV_ROOT/bin:$VSCODE_PATH:$PATH"
